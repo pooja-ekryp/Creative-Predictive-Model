8391340bc8240a38bd7140f76dd792af38578f9a
pooja-ekryp <51340852+pooja-ekryp@users.noreply.github.com>
2019-07-02 16:13:16 -0700

    Add files via upload

---
+++ Asset_information_final.sql
@@ -0,0 +1,7 @@
+select 
+i.customer_asset_identifier,i.serial_number,i.installed_date, f.model_name,f.model_group_id,
+f.category_id,f.type_id, f.capacity, i.service_provider_id,r.site_name, r.ekryp_partner_id, c.company_name 
+from  asset_ib_info as i 
+left join asset_features as f on f.id = i.id
+inner join company_record as c on i.customer_id = c.company_id
+inner join  site_record as r on r.site_id = i.site;
